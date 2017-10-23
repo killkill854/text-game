1f524e2554c6b2bcd6d1ff75ec4ca04047fd7dd2
stepan <stepan200409@gmail.com>
2017-10-23 17:43:12 +0300

    Banana_MAcho!!!!!

---
+++ src/com/banana/textgame/Main.java
@@ -0,0 +1,47 @@
+package com.banana.textgame;
+
+public class Main {
+
+    /*
+     * Главный метод.
+     */
+    public static void main(String[] args) {
+        // вызывает метод start()
+        new Main().start();
+    }
+
+    /*
+     * Метод с логикой игры.
+     */
+    private void start() {
+        onStart();
+        for (int i = 1; i <= 5; ++i) {
+            onNewDay(i);
+        }
+        onFinish();
+    }
+
+    /*
+     * Метод вызывается один раз при старте игры.
+     */
+    void onStart() {
+        System.out.println("Banan_MAcxho");
+
+    }
+
+    /*
+     * Метод вызывается каждый игровый день.
+     * Единственный параметр: dayNumber - номер текущего игрового дня.
+     */
+    void onNewDay(int dayNumber) {
+
+    }
+
+    /*
+     * Метод вызывается по завершению игры.
+     */
+    void onFinish() {
+
+    }
+
+}
